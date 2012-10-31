72787a9165622905d427b0d43a0d6d218fdaae9f
SakaiTomoyuki@MacBook <hzd02765@nifty.com>
2012-10-31 12:31:09 +0900

    page 69

---
+++ chapter03/Var4.java
@@ -0,0 +1,10 @@
+public class Var4{
+    public static void main(String[] args){
+	char a = 'A';
+	char alpha = 'a';
+	char hiroshi = '浩';
+	System.out.println("変数の a 値は" + a);
+	System.out.println("変数の alpha 値は" + alpha);
+	System.out.println("変数の hiroshi 値は" + hiroshi);
+    }
+}
