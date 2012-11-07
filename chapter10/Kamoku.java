293680fe0d767e7becf60e12a484d1ba8ea4b332
SakaiTomoyuki@winxpmacbook <hzd02765@nifty.com>
2012-11-07 15:45:56 +0900

    page 267

---
+++ chapter10/Kamoku.java
@@ -0,0 +1,13 @@
+public class Kamoku {
+	String namae;
+	int tensuu;
+	
+	public Kamoku(String namae, int tensuu) {
+		this.namae = namae;
+		this.tensuu = tensuu;
+	}
+	
+	public String toString(){
+		return this.namae + "‚Í" + this.tensuu + "“_";
+	}
+}
