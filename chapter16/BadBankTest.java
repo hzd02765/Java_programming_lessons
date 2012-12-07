670b5768c8080433cc330928c166cbe606e143b9
tomoyuki@iMac <hzd02765@nifty.com>
2012-12-07 11:20:33 +0900

    page 174

---
+++ chapter16/BadBankTest.java
@@ -0,0 +1,17 @@
+public class BadBankTest extends Thread {
+    BadBank bank;
+    public BadBankTest(BadBank bank){
+	this.bank = bank;
+    }
+    public void run(){
+	while (true){
+	    bank.addMoney(100);
+	    bank.addMoney(-100);
+	}
+    }
+    public static void main(String[] args){
+	BadBank bank = new BadBank();
+	new BadBankTest(bank).start();
+	new BadBankTest(bank).start();
+    }
+}
---
+++ chapter16/GoodBankTest.java
@@ -0,0 +1,17 @@
+public class GoodBankTest extends Thread {
+	GoodBank bank;
+	public GoodBankTest(GoodBank bank){
+		this.bank = bank;
+	}
+	public void run(){
+		while (true){
+			bank.addMoney(100);
+			bank.addMoney(-100);
+		}
+	}
+	public static void main(String[] args) {
+		GoodBank bank = new GoodBank();
+		new GoodBankTest(bank).start();
+		new GoodBankTest(bank).start();
+	}
+}
